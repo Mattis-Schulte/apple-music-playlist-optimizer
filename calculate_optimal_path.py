ceac8d14af18c2299ca0e1521b5ffc3002b355c6
Mattis Schulte <34488470+Mattis-Schulte@users.noreply.github.com>
2024-01-25 08:24:03 +0100

    simplification

--- calculate_optimal_path.py
+++ calculate_optimal_path.py
@@ -46,58 +46,44 @@ def graph_data(df: pd.DataFrame) -> nx.Graph:
     return G
 
 
-def find_best_path(G: nx.Graph, num_attempts: int = 50) -> list:
+def find_path(G: nx.Graph, current_node: str) -> tuple:
     """
     Find the best path in the graph using a greedy algorithm and jump to the next unvisited node with the highest degree if necessary.
     """
-    def find_path_from_node(G: nx.Graph, current_node: str) -> tuple:
-        path = [current_node]
-        visited = set(path)
-        total_weight = 0
-
-        while len(visited) < len(G.nodes()):
-            # Find the heaviest unvisited neighbor
-            neighbors = [(neighbor, G[current_node][neighbor]['weight'])
-                         for neighbor in G.neighbors(current_node)
-                         if neighbor not in visited]
-            if neighbors:
-                next_node, weight = max(neighbors, key=lambda x: x[1])
-                total_weight += weight
-            else:
-                # If there are no unvisited neighbors, jump to the next unvisited
-                # node with the highest degree that's not visited yet
-                remaining_nodes = [node for node in sorted_nodes if node not in visited]
-                if not remaining_nodes:
-                    break
-                next_node = remaining_nodes[0]
-                # Add the average weight of the graph in case of a jump
-                total_weight += avg_weight
-
-            # Visit the next node
-            path.append(next_node)
-            visited.add(next_node)
-            current_node = next_node
-
-        return path, total_weight
+    path = [current_node]
+    visited = set(path)
+    total_weight, total_jumps = 0, 0
 
     # Pre-calculate some graph properties
     sorted_nodes = sorted(G.nodes(), key=lambda n: G.degree(n, weight='weight'), reverse=True)
     avg_weight = sum(data['weight'] for _, _, data in G.edges(data=True)) / G.number_of_edges()
 
-    # Run the find_path_from_node function num_attempts times
-    best_path = []
-    best_weight = -1
-    for _ in range(num_attempts):
-        # Start from a random node for each attempt
-        start_node = random.choice(list(G.nodes()))
-        path, total_weight = find_path_from_node(G, start_node)
-        print(f'Found path with length {len(path)} and weight {total_weight}...')
-        if total_weight > best_weight or (total_weight == best_weight and len(path) > len(best_path)):
-            best_path = path
-            best_weight = total_weight
-    
-    print(f'Chose best path with length {len(best_path)} and weight {best_weight}.')
-    return best_path
+    while len(visited) < len(G.nodes()):
+        # Find the heaviest unvisited neighbor
+        neighbors = [(neighbor, G[current_node][neighbor]['weight'])
+                        for neighbor in G.neighbors(current_node)
+                        if neighbor not in visited]
+        if neighbors:
+            next_node, weight = max(neighbors, key=lambda x: x[1])
+            total_weight += weight
+        else:
+            # If there are no unvisited neighbors, jump to the next unvisited
+            # node with the highest degree that's not visited yet
+            remaining_nodes = [node for node in sorted_nodes if node not in visited]
+            if not remaining_nodes:
+                break
+            next_node = remaining_nodes[0]
+            # Add the average weight of the graph in case of a jump
+            total_weight += avg_weight
+            total_jumps += 1
+
+        # Visit the next node
+        path.append(next_node)
+        visited.add(next_node)
+        current_node = next_node
+
+    print(f'Found path with {len(path)} nodes, {total_jumps} jumps and a total weight of {total_weight}.')
+    return path, total_weight
 
 
 def export_graph(G: nx.Graph, export_path: str):
@@ -125,8 +111,8 @@ def export_path(df: pd.DataFrame, path: list, export_path: str):
 
 
 if __name__ == '__main__':
-    df = preprocess_data(pd.read_sql('SELECT * FROM crossreference', sqlite3.connect('identified_songs.sqlite3')))
-    G = graph_data(df)
-    path = find_best_path(G)
-    export_path(df, path, 'calculated_path.sqlite3')
-    # export_graph(G, 'graph.csv') # Uncomment this line to export the graph as a csv file for visualization purposes in Cosmograph
+    df = preprocess_data(df=pd.read_sql('SELECT * FROM crossreference', sqlite3.connect('identified_songs.sqlite3')))
+    G = graph_data(df=df)
+    path = find_path(G=G, current_node=random.choice(list(G.nodes())))
+    export_path(df=df, path=path[0], export_path='calculated_path.sqlite3')
+    # export_graph(G=G, export_path='graph.csv') # Uncomment this line to export the graph as a csv file for visualization purposes in Cosmograph
